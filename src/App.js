6c8c0800ffbcb894f2ac6493c74bed5c7a917f66
Adam Flett <abflett@yahoo.ca>
2019-07-31 11:19:27 -0500

    display a test user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github p" />
-        <h1>Hello from react</h1>
+        <UserItem />
       </div>
     );
   }
