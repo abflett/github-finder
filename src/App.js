6cc04b45d02754f2aed2c08b335dd532a73fd309
Adam Flett <abflett@yahoo.ca>
2019-07-31 10:18:47 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <h1>Hello from react</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
