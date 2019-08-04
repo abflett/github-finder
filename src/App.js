47cb60e68abc5aaabcd5ca7e8747abc0a78a3379
Adam Flett <abflett@yahoo.ca>
2019-08-05 00:32:01 +0800

    adding user section

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
+import User from "./components/users/User";
 
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -29,6 +31,18 @@ class App extends Component {
       users: res.data.items,
       loading: false
     });
+  };
+
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
   };
 
   clearUsers = () => {
@@ -53,7 +67,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { user, users, loading } = this.state;
     return (
       <Router>
         <div className="App">
@@ -77,6 +91,18 @@ class App extends Component {
                 )}
               />
               <Route exact path="/about" render={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
