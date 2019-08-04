e26597f8d1268393d3e321128d43466e1f28900e
Adam Flett <abflett@yahoo.ca>
2019-08-04 23:51:00 +0800

    about page with link on nav

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 
 import "./App.css";
 
@@ -75,6 +76,7 @@ class App extends Component {
                   </Fragment>
                 )}
               />
+              <Route exact path="/about" render={About} />
             </Switch>
           </div>
         </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const Navbar = ({ icon, title }) => {
@@ -8,6 +9,18 @@ const Navbar = ({ icon, title }) => {
         <i className={icon} />
         {title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/" alt="">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" alt="">
+            About
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
